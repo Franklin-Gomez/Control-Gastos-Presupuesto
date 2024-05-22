64fa44604c673417f1932307de7ed6c19286fd0b
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-22 10:00:28 -0500

    add html form component

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react"
+
 
 export default function BudgetForm() {
-  return (
-    <div>
-        <h1> desde Budget Form </h1>
-    </div>
-  )
+
+    const [ budget , setBudget ] = useState(0)
+
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => { 
+        setBudget( +e.target.value )
+    }
+
+    return (
+        <div>
+            <form className="space-y-5">
+
+                <div className=" flex flex-col space-y-5">
+                    <label htmlFor="budget" className=" text-4xl text-blue-600 font-bold text-center">
+                        Definir Presupuesto
+                    </label>
+
+                    <input
+                        id="budget"
+                        type="number"
+                        className=" w-full bg-white border border-gray-200 p-2"
+                        placeholder="Define tu Presupuesto"
+                        name="budget"
+                        onChange={handleChange}
+                    />
+                    
+                </div>
+
+                <input 
+                    type="submit" 
+                    value='Definit Presupuesto'
+                    className=" bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+                />
+            </form>
+        </div>
+    )
 }
