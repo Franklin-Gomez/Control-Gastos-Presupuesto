caaf1997390ef317dcbc48fb46aa7118de98c59b
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-24 14:25:26 -0500

    sho new state , show modal window

--- src/reducers/budget-recuer.ts
+++ src/reducers/budget-recuer.ts
@@ -1,12 +1,15 @@
 export type BudgetActions = 
-    { type : 'add-budget' , payload : { budget : number }}
+    { type : 'add-budget' , payload : { budget : number }} |
+    { type : 'show-modal'}
 
 export type BudgetType = { 
     budget : number
+    modal : boolean
 }
 
 export const initialState : BudgetType = {
-    budget : 0
+    budget : 0,
+    modal : false
 }
 
 export const BudgetRecuder = (
@@ -20,6 +23,13 @@ export const BudgetRecuder = (
         return {
             ...state,
             budget : action.payload.budget
+        }
+    }
+
+    if( action.type == 'show-modal') {
+        return {
+            ...state,
+            modal : true
         }
     }
 
