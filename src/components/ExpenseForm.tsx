174ff2c82ea99bf93b581c1d0c9f828ced684858
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-27 10:55:23 -0500

    state expense and types

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -3,92 +3,103 @@ import { useState } from 'react';
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css'
 import 'react-calendar/dist/Calendar.css'
+import { DraftExpense } from "../types";
 
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function ExpenseForm() {
-  return (
-    <form action="space-y-5">
-        <legend
-            className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
-        
-        > Nuevo Gasto </legend>
 
-        <div className="flex flex-col gap-2">
-            <label 
-                htmlFor="expenseName"
-                className="text-xl"
-            > Nombre Gasto : </label>
+    const [ expense , setExpense ] = useState<DraftExpense>({
+        amount : 0,
+        expenseName : '',
+        category : '',
+        date : new Date()
+    })
 
-            <input
-                type="text"
-                id="expenseName"
-                placeholder="Añáde el Nombre del gasto"
-                className="bg-slate-100 p-2"
-                name="expenseName"
-            />    
-        </div>
-        
-        <div className="flex flex-col gap-2">
-            <label 
-                htmlFor="amount"
-                className="text-xl"
-            > Cantidad del  Gasto : </label>
 
-            <input
-                type="number"
-                id="amount"
-                placeholder="Añáde la cantidad del gasto : ej. 300"
-                className="bg-slate-100 p-2"
-                name="amount"
-            />    
-        </div>
-
-        <div className="flex flex-col gap-2">
-            <label 
-                htmlFor="category"
-                className="text-xl"
-            > Categoria del Gasto : </label>
-
-            <select
-                id="category"
-                className="bg-slate-100 p-2 mb-4"
-                name="category"
-            > 
-
-                <option value=""> -- Seleccione -- </option>
-                {categories.map( category => ( 
-                    <option
-                        key={category.id}
-                        value={category.id}
-                    >{category.name}</option>
-                ))}
-
-            </select>
-        </div>
-
-        <div className="flex flex-col gap-2">
-            <label 
-                htmlFor="date" 
-                className="text-xl"
-            > Fecha Gasto : </label>
-
-            <DatePicker
-                className='bg-slate-100 p-2 border-0'
-            />  
+    return (
+        <form action="space-y-5">
+            <legend
+                className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
             
-        </div>
+            > Nuevo Gasto </legend>
 
-        <input
-            type="submit"
-            className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-            value={'Registrar Gasto'}
-        />
+            <div className="flex flex-col gap-2">
+                <label 
+                    htmlFor="expenseName"
+                    className="text-xl"
+                > Nombre Gasto : </label>
+
+                <input
+                    type="text"
+                    id="expenseName"
+                    placeholder="Añáde el Nombre del gasto"
+                    className="bg-slate-100 p-2"
+                    name="expenseName"
+                    value={expense.expenseName}
+                />    
+            </div>
+            
+            <div className="flex flex-col gap-2">
+                <label 
+                    htmlFor="amount"
+                    className="text-xl"
+                > Cantidad del  Gasto : </label>
+
+                <input
+                    type="number"
+                    id="amount"
+                    placeholder="Añáde la cantidad del gasto : ej. 300"
+                    className="bg-slate-100 p-2"
+                    name="amount"
+                    value={expense.amount}
+                />    
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label 
+                    htmlFor="category"
+                    className="text-xl"
+                > Categoria del Gasto : </label>
+
+                <select
+                    id="category"
+                    className="bg-slate-100 p-2 mb-4"
+                    name="category"
+                    value={expense.category}
+                > 
+
+                    <option value=""> -- Seleccione -- </option>
+                    {categories.map( category => ( 
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >{category.name}</option>
+                    ))}
+
+                </select>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label 
+                    htmlFor="date" 
+                    className="text-xl"
+                > Fecha Gasto : </label>
+
+                <DatePicker
+                    className='bg-slate-100 p-2 border-0'
+                    value={expense.date}
+                />  
+
+            </div>
+
+            <input
+                type="submit"
+                className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+                value={'Registrar Gasto'}
+            />
 
 
-    </form>
-  )
+        </form>
+    )
 }
---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+// types para el registro  gruardado
+export type Expense = { 
+    id : string
+    expenseName : string
+    amount : number 
+    category : string 
+    date : Value
+}
+
+//  no le añáde el id , por que se un registro temporal
+export type DraftExpense = Omit<Expense, 'id'>
+
+// types de la dependencia de date
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+// types para la database
+export type category = { 
+    id : string
+    name : string 
+    icon : string 
+}
