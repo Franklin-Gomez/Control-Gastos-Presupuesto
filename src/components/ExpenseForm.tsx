d0005437f4155bf19a99464c708d3222574c5d4f
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-30 09:00:53 -0500

    text in title and input conditional

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -82,7 +82,7 @@ export default function ExpenseForm() {
             <legend
                 className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
             
-            > Nuevo Gasto </legend>
+            > { state.editingId ? 'Guardar Cambios' : 'Guardar Nuevo Gasto'}  </legend>
 
             { error && <ErrorMessage>{error}</ErrorMessage>}
 
@@ -163,7 +163,7 @@ export default function ExpenseForm() {
             <input
                 type="submit"
                 className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-                value={'Registrar Gasto'}
+                value={ state.editingId ? 'Guardar Cambios' : 'Guardar Nuevo Gasto'} 
             />
 
 
