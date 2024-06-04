232f2e643a2ed89066045f088048ceb1febc149a
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-04 14:27:26 -0500

    componente formPresupuesto ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./Components/Header"
+import FormPresupuesto from "./Components/FormPresupuesto"
 
 function App() {
   return (
     <>
       <Header/>
+
+      <FormPresupuesto/>      
       
     </>
   )
--- src/Components/FormPresupuesto.tsx
+++ src/Components/FormPresupuesto.tsx
@@ -1,7 +1,18 @@
 export default function FormPresupuesto() {
   return (
-    <div>
-      
+    <div className=" my-10 items-center max-w-3xl mx-auto shadow-xl">
+
+        <form action="" className=" space-y-5 bg-white p-10 rounded-xl">
+
+            <div className=" flex flex-col ">
+                <label htmlFor="" className=" text-blue-600 font-bold mb-4 text-center text-4xl"> Definir Presupuesto</label>
+                <input type="number" className=" px-2 py-1 w-auto rounded border border-blue-600 " placeholder="Define tu Presupuesto"/>
+            </div>
+
+            <input type="submit" value={'Definir Presupuesto'} className=" py-3 w-full bg-blue-600 text-white font-bold uppercase " />
+
+        </form>
+
     </div>
   )
 }
