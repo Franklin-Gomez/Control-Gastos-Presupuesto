7731408dcd918678211eebc186564da58c8ae740
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-05 10:06:48 -0500

    fix context

---
+++ src/Context/BudgetContext.tsx
@@ -0,0 +1,34 @@
+import { Children, createContext ,  useReducer } from "react";
+import { BudgetReducers , initialState , budgetTypes , BubdgetActions } from "../Reducer/budget-reducers";
+import { ReactNode } from "react";
+
+type BudgetContextProps = { 
+    state : budgetTypes
+    dispatch : React.Dispatch<BubdgetActions>
+}
+
+type BudgetProviderProps = { 
+    children : ReactNode
+}
+
+
+// Context
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+// Provider
+export const BudgetProvider = ( { children } : BudgetProviderProps ) => { 
+
+    const [ state , dispatch ] = useReducer( BudgetReducers , initialState )
+
+    return (
+        
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
+        >
+            { children }
+        </BudgetContext.Provider>
+    )
+}
