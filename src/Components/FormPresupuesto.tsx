229a055f5206f5ae409161caade483a7a8c46278
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-05 11:36:05 -0500

    working on budgetForm

--- src/Components/FormPresupuesto.tsx
+++ src/Components/FormPresupuesto.tsx
@@ -1,17 +1,40 @@
-export default function FormPresupuesto() {
-  return (
-    <div className=" my-10 items-center max-w-3xl mx-auto shadow-xl">
+import { useMemo, useState } from "react"
+import { useBudget } from "../CustomHook/useContext"
 
-      <form action="" className=" space-y-5 bg-white p-10 rounded-xl">
+export default function FormPresupuesto() {
+
+  const [ budget , setBudget ] = useState(0)
+  const { dispatch } = useBudget()
+
+  const handleChange = ( e : React.ChangeEvent<HTMLInputElement> ) => { 
+    setBudget( +e.target.value )
+  }
+
+  const isValid = useMemo(() => { 
+
+    return budget == 0 ? true : false 
+
+  } , [ budget ] )
+
+  const handleSubmit = ( e : React.FormEvent<HTMLFormElement> ) => { 
+
+    e.preventDefault();
+
+  }
+
+  return (
+    <div className=" my-10 items-center max-w-3xl mx-auto shadow-xl" >
+
+      <form action="" className=" space-y-5 bg-white p-10 rounded-xl" onSubmit={ handleSubmit} >
 
         <div className=" flex flex-col ">
 
           <label htmlFor="" className=" text-blue-600 font-bold mb-4 text-center text-4xl"> Definir Presupuesto</label>
-          <input type="number" className=" px-2 py-1 w-auto rounded border border-blue-600 " placeholder="Define tu Presupuesto"/>
+          <input type="number" className=" px-2 py-1 w-auto rounded border border-blue-600 " placeholder="Define tu Presupuesto" onChange={  handleChange  }/>
 
         </div>
 
-        <input type="submit" value={'Definir Presupuesto'} className=" py-3 w-full bg-blue-600 text-white font-bold uppercase " />
+        <input type="submit" value={'Definir Presupuesto'} className=" py-3 w-full bg-blue-600 text-white font-bold uppercase disabled:bg-blue-300" disabled= {isValid}  />
 
       </form>
 
