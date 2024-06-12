57012aa4baf0671ef4fa0a7dc3828cc18ccb826f
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-12 11:34:10 -0500

    format date

--- src/Components/ExpenseDetails.tsx
+++ src/Components/ExpenseDetails.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react"
 import { Expense } from "../Types"
 import { categories } from "../data/db"
+import { formatDate } from "../helpers"
 
 type ExpenseDetailsProps = { 
     expense : Expense
@@ -12,8 +13,6 @@ export default function ExpenseDetails( { expense } : ExpenseDetailsProps) {
 
     const categoryInfo = useMemo(() => categories.filter(( categoria ) => categoria.id == expense.category)[0] , [expense])
 
-    console.log( categoryInfo )
-
     return (
         <div className="flex gap-5 w-full items-center border-b-2 border-blue-600 p-4" key={expense.id}>
 
@@ -22,7 +21,7 @@ export default function ExpenseDetails( { expense } : ExpenseDetailsProps) {
             <div className="w-full gap-y-2">
                 <h6 className="uppercase font-bold"> { categoryInfo.name} </h6>
                 <p> {expense.nameExpense} </p>
-                <time> {} </time>
+                <time> { formatDate( expense.date!.toString() )} </time>
             </div>
 
             <p className=" text-2xl flex font-bold">{ expense.amount }</p>
